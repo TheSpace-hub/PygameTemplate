de9d2347ba22594bc818e8f4861792dddc279efa
TheSpace-hub <78466469+TheSpace-hub@users.noreply.github.com>
2025-08-12 02:31:27 +0300

    Add local _scale and _angle fields

--- src/sprites/image.py
+++ src/sprites/image.py
@@ -13,11 +13,28 @@ if TYPE_CHECKING:
 
 class Image(Sprite):
     """Sprite class for loading and displaying images.
+
+    Attributes:
+        _origin: The original image is unchanged.
+        _scale: Current image scale
+        _angle: The current tilt of the image in degrees.
     """
 
     def __init__(self, app: 'App', position: Vector2, path: str, scale: tuple[int, int] | None = None):
+        """Initialization.
+
+        Args:
+            app: The main class of the application.
+            position: The position of the sprite on the screen.
+            path: The path to the image.
+            scale: Image scale.
+        """
         super().__init__(app, (0, 0), position)
-        self.image = pg.image.load(path)
+        self._origin = pg.image.load(path)
+        self.image = self._origin.copy()
+
+        self._scale: tuple[int, int] = self.image.get_size()
+        self._angle: float = 0
 
         if scale is not None:
             self.change_scale(scale)
@@ -31,20 +48,8 @@ class Image(Sprite):
         Returns:
             The image itself.
         """
-        self.image = pg.transform.scale(self.image, scale)
-        return self
-
-    def flip(self, x: bool, y: bool) -> Self:
-        """Flip the image.
-
-        Args:
-            x: Reflect the image on the x-axis
-            y: Reflect the image on the y-axis
-
-        Returns:
-            The image itself.
-        """
-        self.image = pg.transform.flip(self.image, x, y)
+        self._scale = scale
+        self.image = pg.transform.scale(self._origin, scale)
         return self
 
     def rotate(self, angle: float) -> Self:
@@ -56,10 +61,11 @@ class Image(Sprite):
         Returns:
             The image itself.
         """
-        self.image = pg.transform.rotate(self.image, angle)
+        self._angle = angle
+        self.image = pg.transform.rotate(pg.transform.scale(self._origin, self._scale), angle)
         return self
 
-    async def update_view(self):
+    def update_view(self):
         pass
 
     async def update(self):
