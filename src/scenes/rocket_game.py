d524272dee6dd233f4469685103dc4e64f1fe358
TheSpace-hub <78466469+TheSpace-hub@users.noreply.github.com>
2025-08-12 02:56:09 +0300

    Sun animation

--- src/scenes/rocket_game.py
+++ src/scenes/rocket_game.py
@@ -1,8 +1,10 @@
 """A scene module with a rocket game.
 """
 import os.path
+import time
 from typing import TYPE_CHECKING
 from math import atan2, pi
+from random import randint
 
 import pygame as pg
 from pygame import Vector2
@@ -22,16 +24,18 @@ class RocketGame(Scene):
     def __init__(self, app: 'App'):
         super().__init__(app)
         self.rocket_velocity: Vector2 = Vector2(5, 0)
+        self.previous_turn_of_sun: float = 0
 
     async def boot(self):
         self.add_sprite('sun', Image(self.app, Vector2(904, 484), os.path.join('assets', 'images', 'sun.png'),
                                      (56, 56)))
         self.add_sprite('rocket',
-                        Image(self.app, Vector2(400, 100), os.path.join('assets', 'images', 'rocket_engine_on.png'),
+                        Image(self.app, Vector2(400, 100), os.path.join('assets', 'images', 'rocket.png'),
                               (50, 96)))
 
     async def update(self):
         self.physics()
+        self.sun_animation()
 
         if pg.key.get_pressed()[pg.K_ESCAPE]:
             self.app.quit()
@@ -46,6 +50,15 @@ class RocketGame(Scene):
         rocket_center_position: Vector2 = rocket.position + Vector2(rocket.image.get_size()[0] // 2,
                                                                     rocket.image.get_size()[1] // 2)
         self.rocket_velocity += (Vector2(960, 540) - rocket_center_position) * .0001
+
+    def sun_animation(self):
+        """Rotates the sun making a kind of animation.
+        """
+        if time.time() - self.previous_turn_of_sun >= .25:
+            sun: Image = self.get_sprite('sun')
+            sun.rotate(90 * randint(0, 3))
+
+            self.previous_turn_of_sun = time.time()
 
     async def enter(self):
         pass
