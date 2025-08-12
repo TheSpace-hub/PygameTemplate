e6270182af490abdb7083795af906e2cd590060c
TheSpace-hub <78466469+TheSpace-hub@users.noreply.github.com>
2025-08-13 02:47:59 +0300

    Quit and quit tip

--- src/scenes/connection_to_service.py
+++ src/scenes/connection_to_service.py
@@ -11,6 +11,7 @@ from typing import TYPE_CHECKING, Optional
 import aiohttp
 from aiohttp import ContentTypeError
 from pygame import Vector2
+import pygame as pg
 
 from src.scene import Scene
 
@@ -32,22 +33,28 @@ class ConnectionToService(Scene):
         self.connection_tasks: dict[str, Optional[Task]] = {}
 
     async def boot(self):
-        self.add_sprite('delay_address', Text(self.app, Vector2(10, 10), '* Connecting to httpbin.org/delay/5',
+        self.add_sprite('tip', Text(self.app, Vector2(10, 10), 'Press escape to quit', color=(128, 128, 128),
+                                    align=TextAlign.LEFT))
+
+        self.add_sprite('delay_address', Text(self.app, Vector2(10, 50), '* Connecting to httpbin.org/delay/5',
                                               align=TextAlign.LEFT))
-        self.add_sprite('delay_waiting', Waiting(self.app, Vector2(45, 50), (400, 30), CompletionStatus.WORKING))
+        self.add_sprite('delay_waiting', Waiting(self.app, Vector2(45, 90), (400, 30), CompletionStatus.WORKING))
 
-        self.add_sprite('error_500_address', Text(self.app, Vector2(10, 110), '* Connecting to httpbin.org/status/500',
+        self.add_sprite('error_500_address', Text(self.app, Vector2(10, 150), '* Connecting to httpbin.org/status/500',
                                                   align=TextAlign.LEFT))
-        self.add_sprite('error_500_waiting', Waiting(self.app, Vector2(45, 150), (400, 30), CompletionStatus.WORKING))
+        self.add_sprite('error_500_waiting', Waiting(self.app, Vector2(45, 190), (400, 30), CompletionStatus.WORKING))
 
-        self.add_sprite('get_uuid_address', Text(self.app, Vector2(10, 210), '* Connecting to httpbin.org/uuid',
+        self.add_sprite('get_uuid_address', Text(self.app, Vector2(10, 250), '* Connecting to httpbin.org/uuid',
                                                  align=TextAlign.LEFT))
-        self.add_sprite('get_uuid_result', Text(self.app, Vector2(45, 290), 'Random UUID: -',
+        self.add_sprite('get_uuid_result', Text(self.app, Vector2(45, 330), 'Random UUID: -',
                                                 align=TextAlign.LEFT))
-        self.add_sprite('get_uuid_waiting', Waiting(self.app, Vector2(45, 250), (400, 30), CompletionStatus.WORKING))
+        self.add_sprite('get_uuid_waiting', Waiting(self.app, Vector2(45, 290), (400, 30), CompletionStatus.WORKING))
 
     async def update(self):
         await self.update_tasks()
+
+        if pg.key.get_pressed()[pg.K_ESCAPE]:
+            self.app.quit()
 
     async def enter(self):
         self.connection_tasks['delay_waiting'] = asyncio.create_task(self.fetch_get('https://httpbin.org/delay/5'))
